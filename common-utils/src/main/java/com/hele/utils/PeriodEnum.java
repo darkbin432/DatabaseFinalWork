78f5552f823d45a45ca3aee4a339085e19151ce3
luowb <1015817997@qq.com>
2019-05-16 20:11:30 +0800

    框架搭建

---
+++ common-utils/src/main/java/com/hele/utils/PeriodEnum.java
@@ -0,0 +1,12 @@
+package com.hele.utils;
+
+/**
+ * PeriodEnum
+ *
+ * @author xuzou
+ * @date 6/16/16
+ * @copyright: copyright @ HeleTech 2016
+ */
+public enum PeriodEnum {
+    DAY,WEEK,MONTH,SEESION,YEAR
+}
